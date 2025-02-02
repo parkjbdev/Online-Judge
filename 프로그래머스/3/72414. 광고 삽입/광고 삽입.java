83cc71be77f591699d2c0ba78b10e1fbf542dfc6
Park JongBeum <dev@parkjb.com>
2025-02-02 15:48:23 +0900

    [level 3] Title: 광고 삽입, Time: 3629.76 ms, Memory: 157 MB -BaekjoonHub

---
+++ 프로그래머스/3/72414. 광고 삽입/광고 삽입.java
@@ -0,0 +1,67 @@
+class Solution {
+    public String solution(String play_time, String adv_time, String[] logs) {
+        return new Prob5_Sliding(play_time, adv_time, logs).solve();
+    }
+}
+
+class Prob5_Sliding
+{
+	int playTime, adTime;
+	int[] timeLine;
+
+	private int getIntTime(String strTime)
+	{
+		int hour = Integer.parseInt(strTime.substring(0, 2));
+		int minute = Integer.parseInt(strTime.substring(3, 5));
+		int second = Integer.parseInt(strTime.substring(6, 8));
+
+		return hour * 3600 + minute * 60 + second;
+	}
+
+	private String getStringTime(int intTime)
+	{
+		int second = intTime % 60;
+		int minute = (intTime / 60) % 60;
+		int hour = (intTime / 3600);
+
+		return String.format("%02d", hour) + ":" +
+				String.format("%02d", minute) + ":" +
+				String.format("%02d", second);
+	}
+
+	public Prob5_Sliding(String playTime, String adTime, String[] logs)
+	{
+		this.playTime = getIntTime(playTime);
+		this.adTime = getIntTime(adTime);
+		this.timeLine = new int[this.playTime + 1];
+
+		for (String log : logs)
+		{
+			int start = getIntTime(log.substring(0, 8));
+			int end = getIntTime(log.substring(9));
+			for (int i = start; i < end; i++) timeLine[i]++;
+		}
+	}
+
+	public String solve()
+	{
+		int result = 0;
+		long max, sum = 0;
+
+		for (int i = 0; i < adTime; i++)	sum += timeLine[i];
+		max = sum;
+
+		for (int start = 0; start + adTime < timeLine.length; start++)
+		{
+			sum = sum - timeLine[start] + timeLine[start + adTime];
+
+			if (max < sum)
+			{
+				max = sum;
+				result = start + 1;
+			}
+		}
+
+		return getStringTime(result);
+	}
+}

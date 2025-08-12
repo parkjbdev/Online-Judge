4b439f24258080d463b8c110c87338a61daed536
Park JongBeum <dev@parkjb.com>
2025-08-12 18:55:40 +0900

    [level 4] Title: 가사 검색, Time: 451.80 ms, Memory: 437 MB -BaekjoonHub

---
+++ 프로그래머스/4/60060. 가사 검색/가사 검색.cpp
@@ -0,0 +1,105 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <memory>
+
+using namespace std;
+
+struct Trie {
+    int cnt;
+    struct Trie* next[26];
+
+    Trie() {
+        cnt = 0;
+        fill_n(next, 26, nullptr);
+    }
+
+    ~Trie() {
+        for (int i = 0;i < 26;i++) {
+            delete next[i];
+        }
+    }
+    
+    // Trie(const Trie&) = delete;
+    // Trie& operator=(const Trie&) = delete;
+
+    void insert(const string& c) {
+        struct Trie* iter = this;
+        iter->cnt++;
+
+        for (int i = 0;i < c.size();i++) {
+            if (!iter->next[c[i] - 'a']) {
+                iter->next[c[i] - 'a'] = new Trie();
+            }
+
+            iter = iter->next[c[i] - 'a'];
+            iter->cnt++;
+        }
+    }
+
+    int count(const string& s) {
+        struct Trie* iter = this;
+        int idx = 0;
+        
+        while (s[idx] != '?') {
+            iter = iter->next[s[idx] - 'a'];
+            if (iter == nullptr) return 0;
+            idx++;
+        }
+        
+        return iter->cnt;
+        
+        // while (iter != nullptr) {
+        //     iter = iter->next[s[idx] - 'a'];
+        //     if (iter != nullptr && s[++idx] == '?') return iter->cnt;
+        // }
+
+        // return 0;
+    }
+};
+
+vector<int> solution(vector<string> words, vector<string> queries) {
+    vector<int> answer;
+    unordered_map<string, int> result_cache;
+    unordered_map<int, struct Trie> fwd, bwd;
+
+    for (auto word: words) {
+        string rev = word;
+        reverse(rev.begin(), rev.end());
+        
+        // if (!fwd[word.size()]) fwd[word.size()] = make_unique<Trie>();
+        // if (!bwd[word.size()]) bwd[word.size()] = make_unique<Trie>();
+
+        fwd[word.size()].insert(word);
+        bwd[word.size()].insert(rev);
+    }
+
+    for (auto query: queries) {
+        int cnt = 0;
+        
+        auto find = result_cache.find(query);
+        
+        if (find != result_cache.end()) {
+            answer.push_back((*find).second);
+            continue;
+        }
+        
+        if (query[0] == '?') {
+            string rev = query;
+            reverse(rev.begin(), rev.end());
+        	if (bwd.find(query.size()) != bwd.end())
+        	// if (bwd.count(query.size()))
+            	cnt = bwd[query.size()].count(rev);
+        } else {
+        	if (fwd.find(query.size()) != fwd.end())
+        	// if (fwd.count(query.size()))
+            	cnt = fwd[query.size()].count(query);
+        }
+        
+        result_cache[query] = cnt;
+        answer.push_back(cnt);
+    }
+
+    return answer;
+}

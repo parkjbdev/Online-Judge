f46c98796f1c6c066805c5133120bcc12ec6be62
Park JongBeum <dev@parkjb.com>
2025-09-23 16:43:13 +0900

    [Gold I] Title: 2048 (Easy), Time: 380 ms, Memory: 118612 KB -BaekjoonHub

---
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
@@ -0,0 +1,65 @@
+from collections import deque
+
+N = int(input())
+MAP = [list(map(int, input().split())) for _ in range(N)]
+
+def move(dx, dy, MAP):
+    TMP_MAP = [row[:] for row in MAP]
+    if (dx, dy) == (0, 1):
+        # 좌우반전
+        TMP_MAP = [row[::-1] for row in TMP_MAP]
+    elif (dx, dy) == (-1, 0):
+        # 반시계 90도
+        TMP_MAP = list(map(list, zip(*TMP_MAP[::-1])))
+    elif (dx, dy) == (1, 0):
+        # 시계 90도
+        TMP_MAP = list(map(list, zip(*TMP_MAP)))[::-1]
+
+    for i in range(N):
+        tmp = deque([x for x in TMP_MAP[i] if x != 0])
+        res = []
+        while tmp:
+            n = tmp.popleft()
+            if tmp and n == tmp[0]:
+                tmp.popleft()
+                res.append(n * 2)
+            else:
+                res.append(n)
+
+        TMP_MAP[i] = res + [0] * (N - len(res))
+
+    # 원상복구
+    if (dx, dy) == (0, 1):
+        # 좌우반전
+        TMP_MAP = [row[::-1] for row in TMP_MAP]
+    elif (dx, dy) == (-1, 0):
+        # 시계 90도
+        TMP_MAP = list(map(list, zip(*TMP_MAP)))[::-1]
+    elif (dx, dy) == (1, 0):
+        # 반시계 90도
+        TMP_MAP = list(map(list, zip(*TMP_MAP[::-1])))
+
+    return TMP_MAP
+
+
+def move_i(i, MAP):
+    if i == 0:
+        return move(-1, 0, MAP)
+    elif i == 1:
+        return move(1, 0, MAP)
+    elif i == 2:
+        return move(0, -1, MAP)
+    elif i == 3:
+        return move(0, 1, MAP)
+
+answer = 0
+for i in range(4):
+    for j in range(4):
+        for k in range(4):
+            for l in range(4):
+                for m in range(4):
+                    RES = move_i(m, move_i(l, move_i(k, move_i(j, move_i(i, MAP)))))
+                    for n in range(N):
+                        answer = max(answer, max(RES[n]))
+
+print(answer)

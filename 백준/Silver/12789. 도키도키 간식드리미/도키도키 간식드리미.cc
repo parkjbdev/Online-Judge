b40046058de40a014c4e62aec29098c7580fda83
Park JongBeum <dev@parkjb.com>
2026-02-05 23:10:00 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.cc
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <stack>
-#include <deque>
+#include <vector>
 
 using namespace std;
 
@@ -8,32 +8,23 @@ int main() {
     int N;
     cin >> N;
 
-    deque<int> main_line(N);
+    vector<int> main_line(N);
     stack<int> temp_line;
     for (int i = 0; i < N; i++) cin >> main_line[i];
 
     int current = 1;
 
-    while (current < N) {
-        if (!main_line.empty() && main_line.front() == current) {
-            main_line.pop_front();
-            current++;
-        } else if (!temp_line.empty() && temp_line.top() == current) {
+    for (int i = 0; i < N; i++) {
+        temp_line.push(main_line[i]);
+
+        while (!temp_line.empty() && temp_line.top() == current) {
             temp_line.pop();
             current++;
-        } else {
-            while (!main_line.empty()) {
-                temp_line.push(main_line.front());
-                main_line.pop_front();
-                if (temp_line.top() == current) break;
-            }
-            if (main_line.empty() && temp_line.top() != current) {
-                cout << "Sad";
-                return 0;
-            }
         }
     }
 
-    cout << "Nice";
+    if (temp_line.empty()) cout << "Nice";
+    else cout << "Sad";
+
     return 0;
 }

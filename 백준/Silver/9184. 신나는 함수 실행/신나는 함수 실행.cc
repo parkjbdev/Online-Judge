c997110c50c75ea2286e69f47f84b9e735d0efc1
Park JongBeum <dev@parkjb.com>
2026-02-05 17:29:28 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 232 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -4,22 +4,31 @@ using namespace std;
 
 int memo[MAX][MAX][MAX] = {0,};
 
-int w(int a, int b, int c) {
-    if (a <= 0 || b <= 0 || c <= 0) return 1;
-    if (a > 20 || b > 20 || c > 20) return w(20, 20, 20);
+int w(int a, int b, int c);
 
-    if (memo[a][b][c] != 0) return memo[a][b][c];
-
-    if (a < b && b < c) {
-        memo[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(
-                            a, b - 1, c);
-    } else {
-        memo[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1)
-                        -
-                        w(a - 1, b - 1, c - 1);
+int w_wrapper(int a, int b, int c) {
+    if (a <= 0 || b <= 0 || c <= 0 || a > 20 || b > 20 || c > 20) {
+        return w(a, b, c);
     }
 
-    return memo[a][b][c];
+    if (memo[a][b][c] != 0) return memo[a][b][c];
+    return memo[a][b][c] = w(a, b, c);
+}
+
+int w(int a, int b, int c) {
+    if (a <= 0 || b <= 0 || c <= 0) return 1;
+    if (a > 20 || b > 20 || c > 20) return w_wrapper(20, 20, 20);
+
+    if (a < b && b < c) {
+        return w_wrapper(a, b, c - 1) +
+               w_wrapper(a, b - 1, c - 1) -
+               w_wrapper(a, b - 1, c);
+    }
+
+    return w_wrapper(a - 1, b, c) +
+           w_wrapper(a - 1, b - 1, c) +
+           w_wrapper(a - 1, b, c - 1) -
+           w_wrapper(a - 1, b - 1, c - 1);
 }
 
 int main() {
@@ -29,10 +38,10 @@ int main() {
     while (true) {
         int a, b, c;
         cin >> a >> b >> c;
-        int answer = w(a, b, c);
         if (a == -1 && b == -1 && c == -1) break;
 
-        cout << "w(" << a << ", " << b << ", " << c << ") = " << answer << endl;
+        cout << "w(" << a << ", " << b << ", " << c << ") = " <<
+                w_wrapper(a, b, c) << endl;
     }
 
     return 0;

5a0ea6d570c6a72d91e7b120ab827fb5d725a12b
Park JongBeum <dev@parkjb.com>
2026-02-07 01:38:34 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -1,7 +1,2 @@
-#include <iostream>
-
-int main() {
-    int x, y;
-    std::cin >> x >> y;
-    std::cout << "3241"[(x > 0) << 1 | (y > 0)];
-}
+#include<iostream>
+int main(){int x, y;std::cin>>x>>y;std::cout<<"3241"[(x>0)<<1|y>0];}
